fbd48eb6e798a08f66a07b69de32829e76f124aa
Brian E Johnson <bedwardj@gmail.com>
2022-11-25 23:05:25 -0600

    Updated docs\source\conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,26 +3,58 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here.
+import pathlib
+import sys
+
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pywin32'
-copyright = '2022, Mark Hammond'
-author = 'Mark Hammond'
-release = '305'
+project = "pywin32"
+copyright = "2022, Mark Hammond"
+author = "Mark Hammond"
+release = "305"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-
+rst_prolog = """
+.. |Pythonwin| replace:: :program:`Pythonwin`
+.. |Scintilla| replace:: :program:`Scintilla`
+.. _Scintilla: http://www.scintilla.org/
+"""
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+# -- Options for Sphinx autodoc extension ------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
+
+autodoc_mock_imports = [
+    "win32api",
+    "pywintypes",
+    "win32com",
+    "pythoncom",
+    "winerror",
+    "win32file",
+    "win32security",
+    "win32event",
+]
+
+# -- Options for Sphinx todo extension ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
+
+todo_include_todos = True
+# todo_emit_warnings = True
+# todo_link_only = True
